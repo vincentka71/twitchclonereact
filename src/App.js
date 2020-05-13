e0ae4178dd220e37b3cbc45fd4390ef724fb6757
vincentka71 <vincent.kapp@gmail.com>
2020-05-13 15:10:49 +0200

    Ajouts Header et Sidebar basiques

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Header />
+      <Sidebar />
     </div>
   );
 }
